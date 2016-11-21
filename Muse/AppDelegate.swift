0f4540974e62e3da1d69bbc3140fa3a32fd4af2c
John Doe <xzzz9097@gmail.com>
2016-11-21 20:28:37 +0100

    Initial Commit

---
+++ Muse/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Muse
+//
+//  Created by Marco Albera on 21/11/16.
+//  Copyright © 2016 Edge Apps. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Muse/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Muse
+//
+//  Created by Marco Albera on 21/11/16.
+//  Copyright © 2016 Edge Apps. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
