f69c1c2950eb0927c4ae4859bb6d9838b377d9e3
John Doe <xzzz9097@gmail.com>
2016-12-06 18:32:12 +0100

    Simplify activation for remote control buttons Through an MPRemoteCommand extension

--- Muse/WindowController+RemoteCommandCenter.swift
+++ Muse/WindowController+RemoteCommandCenter.swift
@@ -78,21 +78,16 @@ extension WindowController {
     
     func prepareRemoteCommandCenter() {
         // Play/pause toggle
-        remoteCommandCenter.playCommand.isEnabled = true
-        remoteCommandCenter.playCommand.addTarget(self, action: #selector(togglePlayPause(event:)))
-        remoteCommandCenter.pauseCommand.isEnabled = true
-        remoteCommandCenter.pauseCommand.addTarget(self, action: #selector(togglePlayPause(event:)))
+        remoteCommandCenter.playCommand.activate(self, action: #selector(togglePlayPause(event:)))
+        remoteCommandCenter.pauseCommand.activate(self, action: #selector(togglePlayPause(event:)))
         
         // Previous/next track toggle
         // Apparently these work only on 10.12.2+
-        remoteCommandCenter.previousTrackCommand.isEnabled = true
-        remoteCommandCenter.previousTrackCommand.addTarget(self, action: #selector(previousTrack(event:)))
-        remoteCommandCenter.nextTrackCommand.isEnabled = true
-        remoteCommandCenter.nextTrackCommand.addTarget(self, action: #selector(nextTrack(event:)))
+        remoteCommandCenter.previousTrackCommand.activate(self, action: #selector(previousTrack(event:)))
+        remoteCommandCenter.nextTrackCommand.activate(self, action: #selector(nextTrack(event:)))
         
         // Scrub bar control
-        remoteCommandCenter.changePlaybackPositionCommand.isEnabled = true
-        remoteCommandCenter.changePlaybackPositionCommand.addTarget(self, action: #selector(changePlaybackPosition(event:)))
+        remoteCommandCenter.changePlaybackPositionCommand.activate(self, action: #selector(changePlaybackPosition(event:)))
     }
     
 }
