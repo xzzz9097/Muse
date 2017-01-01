941aa38256b1b95fb28bb627f33973225da3c0e6
John Doe <xzzz9097@gmail.com>
2017-01-01 10:18:08 +0100

    Derp

--- Muse/PlayersManager.swift
+++ Muse/PlayersManager.swift
@@ -12,25 +12,6 @@ enum PlayerID {
     case vox
 }
 
-// Extend the dictionary with some useful functions
-/* extension Dictionary where Value: PlayerHelper {
-    
-    // MARK: Extended functions
-    
-    var designatedPlayer: PlayerHelper {
-        // Find the first player that's running and playing
-        for (_, helper) in self {
-            if helper.isAvailable && helper.isPlaying {
-                return helper
-            }
-        }
-        
-        // Return Spotify helper by default
-        return SpotifyHelper.shared
-    }
-    
-} */
-
 class PlayersManager {
     
     // MARK: Constructors
