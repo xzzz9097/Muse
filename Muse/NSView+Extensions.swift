351afe112b58da9052d0c4772ce1c620115c62a9
John Doe <xzzz9097@gmail.com>
2017-08-15 00:03:44 +0200

    Added documentation

--- Muse/NSView+Extensions.swift
+++ Muse/NSView+Extensions.swift
@@ -10,6 +10,12 @@ import Cocoa
 
 extension NSView {
     
+    /**
+     Hides or shows a subview of 'self' and resizes self and the window accordingly
+     - parameter subview: the view whose visibility will be toggled
+     - parameter visible: hide (false) or show (true) the view
+     - parameter animate: when true applies an animation to the hide/show transition
+     */
     func toggleSubviewVisibilityAndResize(subview: NSView?,
                                           visible: Bool,
                                           animate: Bool = true) {
@@ -18,16 +24,22 @@ extension NSView {
         
         let currentlyVisible = subview.isDescendant(of: self)
         
+        // Check if requested visibility is actually different from current before proceeding
         guard visible != currentlyVisible else { return }
         
         if visible {
+            // Show: only update frame size if we're not animating
+            // but add the subview regardless to have a smooth entry animation
             if !animate { self.frame.size.height += subview.frame.size.height }
             self.addSubview(subview)
         } else if !animate {
+            // Hide: only remove the subview and update view frame if we're not animating
             subview.removeFromSuperview()
             self.frame.size.height -= subview.frame.size.height
         }
         
+        // Shift the window to the right position and, if animating, remove the subview
+        // at the end of the transition to have it smoothly disappear
         window.shift(by: subview.frame.size.height,
                      direction: visible ? .up : .down,
                      animate: animate) { if !visible { subview.removeFromSuperview() } }
--- Muse/NSWindow+Extensions.swift
+++ Muse/NSWindow+Extensions.swift
@@ -69,12 +69,21 @@ extension NSWindow {
         } )
     }
     
+    /**
+     Shifts the origin of 'self' by a specified delta and direction, resizing self accordingly
+     to keep original vertical and horizontal dimensions.
+     - parameter value: the position shift delta
+     - parameter direction: the direction the window will be shifted to (up, down, left or right)
+     - parameter animate: when true applies an animation to the shift transition
+     - parameter animationCompletionHandler: a closure to be run when the animation ends
+     */
     func shift(by value: CGFloat,
                direction: NSWindowShiftDirection,
                animate: Bool = false,
                animationCompletionHandler: @escaping () -> () = { } ) {
         var frame = self.frame
         
+        // Edit the frame basing on shift value and direction
         switch direction {
         case .up:
             frame.origin.y    -= value
@@ -90,11 +99,13 @@ extension NSWindow {
             frame.size.width  -= value
         }
         
+        // If not animating immediately set the updated frame on the window
         if !animate {
             self.setFrame(frame, display: true, animate: false)
             return
         }
         
+        // Run the frame update in an animation group if transition is requested
         NSAnimationContext.runAnimationGroup( { context in
             context.duration                = 1/3
             context.allowsImplicitAnimation = true
