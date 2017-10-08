6813e7774ad2cb5dc9142fdf4858c94fc2758fb0
John Doe <xzzz9097@gmail.com>
2017-10-08 11:41:43 +0200

    Better key events detection

---
+++ Muse/Sources/Models/KeyCombination.swift
@@ -0,0 +1,35 @@
+//
+//  KeyCombination.swift
+//  Muse
+//
+//  Created by Marco Albera on 08/10/2017.
+//  Copyright © 2017 Edge Apps. All rights reserved.
+//
+
+import Foundation
+
+struct KeyCombination {
+    
+    let modifiers: NSEventModifierFlags?
+    
+    let keyCode: Int
+    
+    init(_ modifiers: NSEventModifierFlags, _ keyCode: uint16) {
+        self.modifiers = modifiers
+        self.keyCode   = Int(keyCode)
+    }
+    
+    init(_ modifiers: NSEventModifierFlags? = nil, _ keyCode: Int) {
+        self.modifiers = modifiers
+        self.keyCode   = keyCode
+    }
+}
+
+func ~=(lhs: KeyCombination, rhs: KeyCombination) -> Bool {
+    if let modifiers = lhs.modifiers {
+        return rhs.modifiers?.contains(modifiers) ?? false && rhs.keyCode == lhs.keyCode
+    }
+    
+    return rhs.keyCode == lhs.keyCode
+}
+
